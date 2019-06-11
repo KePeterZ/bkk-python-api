2c77193b4f69fba6855f4481570b05dffd98c5da
KePeterZ <46104150+KePeterZ@users.noreply.github.com>
2019-06-11 15:00:35 +0200

    Removed unnecesary code.

--- bkk.py
+++ bkk.py
@@ -32,29 +32,3 @@ def routeDetails(routeId, related="false"):
 def tripDetails(vehicleId="", tripId="", date="20190609"):
     result = call("http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/trip-details.json?key=apaiary-test&version=3&appVersion=apiary-1.0&includeReferences=true&tripId="+tripId+"&date="+date)
     return json.loads(result)["data"]
-
-# stops = search("Raktár utca")["entry"]["stopIds"]
-# for i in stops:
-#     if arrivalsAndDeparturesForStop(i)["entry"]["stopTimes"] != []:
-#         try:
-#             for i in arrivalsAndDeparturesForStop(i)["entry"]["stopTimes"]:
-#                 #print(i["stopHeadsign"], datetime.fromtimestamp(i["arrivalTime"]).strftime("%H:%m"))
-#                 print(i, end="\n\n")
-#         except:
-#             print(end="")
-
-
-megallok = search("Raktár utca")["entry"]["stopIds"]
-megallo_data = []
-for megallo in megallok:
-    megallo_data.append(arrivalsAndDeparturesForStop(megallo, limit="10")["entry"]["stopTimes"])
-
-jaratok = []
-megallo_data = [e for e in megallo_data if e]
-for megallo in megallo_data:
-    for jarat in megallo:
-        jaratok.append(jarat["tripId"])
-
-print(tripDetails(tripId=jaratok[0])["entry"])
-print(jaratok[0])
-    
